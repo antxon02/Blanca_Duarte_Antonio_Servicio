ebd6c463120be34214df8d761acab0fe43b3096e
Antonio <al.antonio.blanca.duarte@iesportada.org>
2026-03-22 17:09:24 +0100

    He creado el Constants.kt

---
+++ app/src/main/java/com/example/blanca_duarte_antonio_servicio/util/Constants.kt
@@ -0,0 +1,9 @@
+package com.example.blanca_duarte_antonio_servicio.util
+
+object Constants {
+    const val URL_CAMBIO = "https://gist.githubusercontent.com/antxon02/63e2f991d38d2ac3c1000712f3a02ef8/raw/a1ca511c43085cc644f0e92fb72c4a7ca3f4b85f/cambio.txt"
+
+    const val PREFS_NAME = "AppPrefs"
+    const val KEY_EXCHANGE_RATE = "EXCHANGE_RATE"
+    const val DEFAULT_RATE = 0.92f
+}
